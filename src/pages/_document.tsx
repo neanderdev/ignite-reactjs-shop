5b1ba40aae6fc7a2ebb5cf4170f05da0f3b890a8
neanderdev <neander.faria@hotmail.com>
2022-09-12 14:05:58 -0300

    Estrutura visual - Configurando Stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { getCssText } from "../styles";
+
 export default function Document() {
     return (
         <Html>
@@ -8,6 +10,8 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+
+                <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
             </Head>
 
             <body>
