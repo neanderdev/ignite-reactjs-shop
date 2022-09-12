79669beff491ff4b6a9cf095a5db396afde70297
neanderdev <neander.faria@hotmail.com>
2022-09-12 16:57:16 -0300

    Produto & Checkout - Dados da compra no sucesso

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -16,11 +16,11 @@ export default async function handler(
     return res.status(400).json({ error: "Price not found." });
   }
 
-  const sucessUrl = `${process.env.NEXT_URL}/success`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
-    success_url: sucessUrl,
+    success_url: successUrl,
     cancel_url: cancelUrl,
     mode: "payment",
     line_items: [
