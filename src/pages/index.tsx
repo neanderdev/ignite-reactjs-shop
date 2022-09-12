ee8fee8e86c514d8e3c6a47907e0eaa110e3351f
neanderdev <neander.faria@hotmail.com>
2022-09-12 15:20:15 -0300

    Integração com Stripe - Data Fetching no Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import camiseta3 from "../assets/3.png";
 
 import 'keen-slider/keen-slider.min.css'
 
-export default function Home() {
+export default function Home(props) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -17,8 +17,11 @@ export default function Home() {
     },
   });
 
+
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
+
       <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="Camiseta 2" />
 
@@ -60,4 +63,14 @@ export default function Home() {
       </Product>
     </HomeContainer>
   )
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
 }
