c68d4c5f26340fa63c73c37bf1b96b4ff21539bf
neanderdev <neander.faria@hotmail.com>
2022-09-12 14:51:44 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,33 @@
-import { styled } from "../styles";
+import Image from "next/future/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import { HomeContainer, Product } from "../styles/pages/home";
 
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-});
+import camiseta1 from "../assets/1.png";
+import camiseta2 from "../assets/2.png";
+import camiseta3 from "../assets/3.png";
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="Camiseta 1" />
 
-      Enviar
-    </Button>
+        <footer>
+          <strong>Camiseta 1</strong>
+
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="Camiseta 2" />
+
+        <footer>
+          <strong>Camiseta 2</strong>
+
+          <span>R$ 89,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
