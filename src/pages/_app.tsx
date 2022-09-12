0e57dd6d2e6d3170b3e32d64e5443318ccc1374f
neanderdev <neander.faria@hotmail.com>
2022-09-12 14:19:38 -0300

    Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import { AppProps } from "next/app";
 
 import { globalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
 
-globalStyles();
+import logoImg from "../assets/logo.svg";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles();
+
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Logo Ignite Shop" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
